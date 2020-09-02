ba98b450f2858c930f2f8577ed687896eef184c6
Objective <phawin@digitopolisstudio.com>
2020-09-02 18:53:33 +0700

    add gin framework and create api login

--- sertis_backend/go.mod
+++ sertis_backend/go.mod
@@ -1,3 +1,9 @@
 module sertis_app
 
 go 1.14
+
+require (
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.6.3
+	go.uber.org/zap v1.16.0
+)
--- sertis_backend/main.go
+++ sertis_backend/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
 func main() {
+	var logger *zap.Logger
+	var err error
+
+	logger, err = zap.NewDevelopment()
+	if err != nil {
+		log.Fatal("cannot initialize logger: ", err)
+	}
+
+	defer logger.Sync()
+
+	zap.ReplaceGlobals(logger)
+
+	router := setupRouter()
+	router.Run(":8880")
+}
+
+func setupRouter() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	zap.L().Info("Start Listen at Port 5550")
+	router.POST("/login", postLogin)
+
+	return router
+}
+
+func postLogin(c *gin.Context) {
 
 }
